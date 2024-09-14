2255569c285cc06bfbf05177b99b84280da40d24
kirgo <kirgotop@gmail.com>
2024-09-14 17:49:23 +0300

    Add project files.

---
+++ ProductsCartWeb/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductsCartWeb.Models
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+        public double? price { get; set; }
+
+        public Product (string name)
+        {
+            Name = name;    
+        }
+    }
+}
---
+++ ProductsCartWeb/ProductsRepository.cs
@@ -0,0 +1,23 @@
+﻿using ProductsCartWeb.Models;
+
+namespace ProductsCartWeb
+{
+    public class ProductsRepository
+    {
+        public static List<Product> Products;
+
+        public static void SeedProducts()
+        {
+            Products = new List<Product>();
+            Products.Add(new Product("Aboba1"));
+            Products.Add(new Product("Aboba2"));
+            Products.Add(new Product("Aboba3"));
+            Products.Add(new Product("Aboba14"));
+            Products.Add(new Product("Abobaasdf sdadf sdadf1"));
+            Products.Add(new Product("Aboba1sd sd s sdsdsd"));
+            Products.Add(new Product("Aboba1sdsdsd "));
+            Products.Add(new Product("Aboba1222"));
+            Products.Add(new Product("Aboba1444"));
+        }
+    }
+}
