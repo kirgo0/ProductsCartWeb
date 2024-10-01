ffae86e99fa86e14388c698ea8a1269f6255f264
kirgo <kirgotop@gmail.com>
2024-10-01 23:13:54 +0300

    Implemented deletion logic

---
+++ ProductsCartWeb/EventArgs/DragAndDropEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using Data.Models;
+
+namespace ProductsCartWeb.EventArgs
+{
+    public class DragAndDropEventArgs
+    {
+        public int IndexToDrop { get; set; }
+        public Product Product { get; set; }
+        public Action SuccessAction { get; set; }
+
+        public DragAndDropEventArgs(int indexToDrop, Product product, Action successAction)
+        {
+            IndexToDrop = indexToDrop;
+            Product = product;
+            SuccessAction = successAction;
+        }
+    }
+}
