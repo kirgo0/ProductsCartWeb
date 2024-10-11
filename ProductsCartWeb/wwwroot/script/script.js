c60b61fdfcd686a7884346c7588e27c999dd3cc6
kirgo <kirgotop@gmail.com>
2024-10-11 17:55:15 +0300

    Refactored logic for naming rows

---
+++ ProductsCartWeb/wwwroot/script/script.js
@@ -0,0 +1,24 @@
+﻿
+function getWidthOfInput(inputEl) {
+    var tmp = document.createElement("span");
+    tmp.className = "input-element tmp-element";
+    tmp.innerHTML = inputEl.value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    document.body.appendChild(tmp);
+    var theWidth = tmp.getBoundingClientRect().width;
+    document.body.removeChild(tmp);
+    return theWidth;
+}
+
+function adjustWidthOfInput(inputEl) {
+    if (inputEl === null) return;
+    inputEl.style.width = getWidthOfInput(inputEl) + "px";
+}
+
+function changeInputFocus(inputEl, isFocused) {
+    if (inputEl === null) return;
+    if (isFocused) {
+        inputEl.blur();
+    } else {
+        inputEl.focus();
+    }
+}
